9e0e1a97646f0a618e839937d48a3e7de32e92bb
VefaKosova <230316001@ogr.cbu.edu.tr>
2024-03-06 17:23:04 +0300

    Initial Commit

---
+++ FaceFacts/FaceFactsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FaceFactsApp.swift
+//  FaceFacts
+//
+//  Created by Vefa Kosova on 6.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct FaceFactsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
